edc014e7de640cc4d08065f43e51e60721b4a327
Soumyadip Ghorai <65104954+soumyadipghorai@users.noreply.github.com>
2024-04-17 14:16:05 +0530

    Update forecasting_utils.py
    group by month and year added

--- Tools/src/forecasting/forecasting_utils.py
+++ Tools/src/forecasting/forecasting_utils.py
@@ -9,7 +9,7 @@ def preprocess(filter_data: list[tuple], feature_parameters: dict) -> pd.DataFra
     all_features = [feature_parameters["feature"]]
     all_features.sort()   
     df = pd.DataFrame(filter_data, columns=['date'] + all_features)
-    df["date"] = pd.to_datetime(df['date'])
+    df["date"] = pd.to_datetime(df['date']).dt.date
     df = df.sort_values(by='date') 
     return df 
     
@@ -32,15 +32,14 @@ def forecast_using_prophet_utils(filter_data: list[tuple], feature_parameters: d
 
         print(forecast_df.tail())
 
-        # fig = px.line(
-        #         forecast_df, x ='date', y = feature_parameters['feature'], 
-        #         title = 'Time Series with Rangeslider', markers = True
-        #     )
+        print("before modify --> ", forecast_df)
+        if feature_parameters["forecasting_date_type"] == "month" or feature_parameters["forecasting_date_type"] == "months" : 
+            forecast_df = forecast_df.groupby(pd.Grouper(key="date", freq="M")).sum()
+        if feature_parameters["forecasting_date_type"] == "year" or feature_parameters["forecasting_date_type"] == "years": 
+            forecast_df = forecast_df.groupby(pd.Grouper(key="date", freq="Y")).sum()
+        print("after modify --> ", forecast_df)
 
-        # fig.update_xaxes(rangeslider_visible=True)
-        # fig.write_html("plots/new_file.html")
-
-        table_creation = forecast_df#.to_dict(orient="tight")
+        table_creation = forecast_df 
         chart_creation = forecast_df.to_dict()
         chart_config = {
             "type": "line",  
@@ -49,4 +48,4 @@ def forecast_using_prophet_utils(filter_data: list[tuple], feature_parameters: d
         }
         return table_creation, chart_creation, chart_config 
     except : 
-        return "couldn't process with Prophet"
+        return "couldn't process with Prophet", None, None
