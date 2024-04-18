46065dd4f672f62e94be0ba0b0b9559e83d89007
indresh <indresh2neel@gmail.com>
2024-04-18 21:15:37 +0530

    first commit

--- celery_queue.py
+++ celery_queue.py
@@ -16,13 +16,13 @@ app = Celery(name='celery_queue',broker=BrokerUrl)
 azure_form= model_utils.AzureDocIntell(**asdict(AzureDocumentInfo()))
 vectorizer=model_utils.ConvertToVector(EMBEDDING,azure_form)
 usecase=UseCaseMongo()
-mongo=mongo_utils.MongoConnect(uri=usecase.uri,db=usecase.db,collection=usecase.collection)
 
 @app.task
 def uploadpdf(uid,file_path):
-    
+    mongo=mongo_utils.MongoConnect(uri=usecase.uri,db=usecase.db,collection=usecase.collection)
+    print(f"========Started Consumption of {uid}=========")
     vectorizer.convert_to_vector(file_path,uid)
-    
+    print(f"=========End Consumption of {uid}=========")
     print("UID",uid)
     mongo.update_data_by_id(uid,{'data_sources':{
         "storage_name":PERSISTANT_DRIVE,
