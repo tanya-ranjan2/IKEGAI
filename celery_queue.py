910f8fae7ac55cf611f5f8cea657d60d30cbe219
indresh <indresh2neel@gmail.com>
2024-04-18 21:21:35 +0530

    first commit

--- celery_queue.py
+++ celery_queue.py
@@ -5,13 +5,13 @@ from dataclasses import asdict
 from DataIngestion.utils import pdf_utils,model_utils,mongo_utils
 from _temp.config import CeleryQueue,RedisBroker,AzureDocumentInfo,EMBEDDING,UseCaseMongo,PERSISTANT_DRIVE
 
-#redis = Redis(**asdict(RedisBroker()))
-#redis.flushall()
-#redis.flushdb()
+redis = Redis(**asdict(RedisBroker()))
+redis.flushall()
+redis.flushdb()
 
-BrokerUrl='pyamqp://guest:guest@20.41.249.147//'
-app = Celery(name='celery_queue',broker=BrokerUrl)
-
+#BrokerUrl='pyamqp://guest:guest@20.41.249.147//'
+#app = Celery(name='celery_queue',broker=BrokerUrl)
+app=Celery(**asdict(CeleryQueue))
 
 azure_form= model_utils.AzureDocIntell(**asdict(AzureDocumentInfo()))
 vectorizer=model_utils.ConvertToVector(EMBEDDING,azure_form)
