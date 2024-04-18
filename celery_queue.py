86c91122ab6cc86efbdd984850a087597c88517c
indresh <indresh2neel@gmail.com>
2024-04-18 21:22:38 +0530

    first commit

--- celery_queue.py
+++ celery_queue.py
@@ -11,7 +11,7 @@ redis.flushdb()
 
 #BrokerUrl='pyamqp://guest:guest@20.41.249.147//'
 #app = Celery(name='celery_queue',broker=BrokerUrl)
-app=Celery(**asdict(CeleryQueue))
+app=Celery(**asdict(CeleryQueue()))
 
 azure_form= model_utils.AzureDocIntell(**asdict(AzureDocumentInfo()))
 vectorizer=model_utils.ConvertToVector(EMBEDDING,azure_form)
