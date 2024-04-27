4dd57d10440386cca49da8174d5c2c944632510f
Soumyadip Ghorai <65104954+soumyadipghorai@users.noreply.github.com>
2024-04-27 19:03:53 +0530

    Update extractor_schema.py

--- app/Tools/schema/extractor_schema.py
+++ app/Tools/schema/extractor_schema.py
@@ -4,3 +4,4 @@ class ExtractKeywords(BaseModel, extra=Extra.allow) :
     user_query: str = Field(description = "original `user input`") 
     default_days: int = Field(description = "minimum days to forecast")
     db_path: str = Field(description = "path of the database for sql query")
+    meta_data_path: str = Field(description= "path of the meta data for the database description")
