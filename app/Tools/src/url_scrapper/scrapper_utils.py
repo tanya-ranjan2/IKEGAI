07c2c5e57b3c01bc71dc670b3bd397f109265103
Shrimanta Satpati <82357659+shrimantasatpati@users.noreply.github.com>
2024-05-01 03:09:08 +0530

    Update scrapper_utils.py

--- app/Tools/src/url_scrapper/scrapper_utils.py
+++ app/Tools/src/url_scrapper/scrapper_utils.py
@@ -4,106 +4,63 @@ from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_openai import AzureOpenAIEmbeddings
 import requests
-from bs4 import BeautifulSoup, NavigableString, Tag
+from bs4 import BeautifulSoup
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.text_splitter import TokenTextSplitter
-from collections import deque
+import html2text
 
 def load_embeddings():
-    # embeddings=HuggingFaceEmbeddings()
     embeddings = AzureOpenAIEmbeddings(
-        azure_endpoint = 'https://openai-lh.openai.azure.com/',
-        openai_api_key = '312ff50d6d954023b8748232617327b6',  
-        deployment = 'LH-embedding',
-        openai_api_version = '2023-06-01-preview',
-        openai_api_type = "azure"
+        azure_endpoint='https://openai-lh.openai.azure.com/',
+        openai_api_key='312ff50d6d954023b8748232617327b6',
+        deployment='LH-embedding',
+        openai_api_version='2023-06-01-preview',
+        openai_api_type="azure"
     )
     return embeddings
 
 def llm():
     llm = AzureChatOpenAI(
-            openai_api_base = "https://openai-lh.openai.azure.com/openai/deployments/LH-GPT",
-            openai_api_version = '2023-06-01-preview',
-            openai_api_key = '312ff50d6d954023b8748232617327b6',
-            temperature = 0, max_tokens = 4096, verbose = True
-        )
+        openai_api_base="https://openai-lh.openai.azure.com/openai/deployments/LH-GPT",
+        openai_api_version='2023-06-01-preview',
+        openai_api_key='312ff50d6d954023b8748232617327b6',
+        temperature=0, max_tokens=4096, verbose=True
+    )
     return llm
-
-def extract_text(node):
-    text = []
-    stack = deque([node])
-    formatting_functions = {
-        'h1': lambda strings: f"\n# {''.join(s.strip() for s in strings)}",
-        'h2': lambda strings: f"\n## {''.join(s.strip() for s in strings)}",
-        'h3': lambda strings: f"\n### {''.join(s.strip() for s in strings)}",
-        'h4': lambda strings: f"\n#### {''.join(s.strip() for s in strings)}",
-        'h5': lambda strings: f"\n##### {''.join(s.strip() for s in strings)}",
-        'h6': lambda strings: f"\n###### {''.join(s.strip() for s in strings)}",
-        'p': lambda strings: f"\n{''.join(s.strip() for s in strings)}",
-        'span': lambda strings: f"\`{''.join(s.strip() for s in strings)}\`",
-        'li': lambda tag: handle_li_tag(tag),
-        'strong': lambda strings: f"\n**{''.join(s.strip() for s in strings)}**"
-    }
-
-    def handle_li_tag(strings):
-        formatted_strings = []
-        for string in strings:
-            if isinstance(string, NavigableString):
-                formatted_strings.append(string.strip())
-            elif isinstance(string, Tag):
-                formatted_strings.append(extract_text(string))
-        return f"\n- {''.join(formatted_strings)}"
-
-    while stack:
-        current_node = stack.pop()
-        if isinstance(current_node, NavigableString):
-            # skipping navigablestring instances
-            continue
-
-        elif isinstance(current_node, Tag):
-            # to skip javascript code and navbar element
-            navbar_elements = ['nav', 'header', 'footer']
-            if current_node.name == 'script' or current_node.name in navbar_elements:
-                continue
-
-            elif current_node.name in formatting_functions:
-                if current_node.name == 'li':
-                    text.append(handle_li_tag(current_node.contents))
-                else:
-                    text.append(formatting_functions[current_node.name](current_node.strings))
-
-            elif current_node.name == 'table':
-                text.append(str(current_node))
-
-            else:
-                # For other non-heading or non-paragraph tags, extend the stack with their children
-                children = reversed(list(current_node.children))
-                stack.extend(children)
-
-    # print('\n'.join(text))
-    return '\n'.join(text)
 
 def get_text_from_url(url):
     try:
         response = requests.get(url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'lxml')
-            content = extract_text(soup.body)
-            return content
-        else:
-            return "failed to fetch content of url"
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        excluded_tagNames = ["header", 'footer', 'nav']
+        excluded_tags = excluded_tagNames or []
+        for tag_name in excluded_tags:
+            for unwanted_tag in soup.find_all(tag_name):
+                unwanted_tag.extract()
+
+        text_content = html2text.html2text(str(soup))
+        # print(text_content)
+        return text_content
+    
     except requests.exceptions.RequestException as e:
-        return "error"
+        # print(f"Error fetching data from {url}: {e}")
+        return f"Error fetching data from {url}: {e}"
 
 def load_vectordb(text, embeddings):
     text_splitter = CharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap  = 100,
-        length_function = len
-    ) 
+        separator = "\n",
+        chunk_size=1000,
+        chunk_overlap=100,
+        length_function=len
+    )
+    
     texts = text_splitter.split_text(text)
     db = Chroma.from_texts(texts, embeddings)
+    # docs = db.similarity_search(query)
     qa = RetrievalQA.from_chain_type(llm=llm(),
-                                    chain_type="stuff",
-                                    retriever=db.as_retriever())
+                                     chain_type="stuff",
+                                     retriever=db.as_retriever(),
+                                    #  return_source_documents=True,
+                                     verbose=True)
     return qa
