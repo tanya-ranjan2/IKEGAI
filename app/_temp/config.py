87f45ddd3e8007a225703ab5d3a0bbd20ff98c09
Bhattacharya <indreshb@kpmg.com>
2024-05-14 14:23:18 +0530

    ENV variables

--- app/_temp/config.py
+++ app/_temp/config.py
@@ -1,11 +1,19 @@
 from dataclasses import dataclass,asdict
 
+###################ENV-SET######################
+ENV="DEV"
 
+# IF someone adds UAT just add UAT Env
+if ENV=="PROD":
+    URI="20.41.249.147"
+elif ENV=="DEV":
+    URI="52.172.103.119"
+#################################
 PERSISTANT_DRIVE="VectorDB"
 STORAGE_DRIVE="Files"
-
-MONGO_URI = "mongodb+srv://ikegai:ikegai%40123456@cluster0.l2apier.mongodb.net"
 EMBEDDING="intfloat/e5-base-v2"
+
+
 
 
 @dataclass
@@ -24,7 +32,7 @@ class AzureDocumentInfo:
     
 @dataclass
 class ChromaClient:
-    host:str="http://20.41.249.147:6062"
+    host:str=f"http://{URI}:6062"
     #host:str="http://192.168.1.2:6062"
     #port:int=6062
 
@@ -32,8 +40,8 @@ class ChromaClient:
 @dataclass
 class CeleryQueue:
     name:str='celery_queue'
-    broker:str='redis://20.41.249.147:6379/0'
-    backend:str="redis://20.41.249.147:6379/1"
+    broker:str='redis://{URI}:6379/0'
+    backend:str="redis://{URI}:6379/1"
     
     
 @dataclass
@@ -49,6 +57,10 @@ class RedisBroker:
 @dataclass
 class UseCaseMongo:
     uri = "mongodb+srv://ikegai:ikegai%40123456@cluster0.l2apier.mongodb.net"
-    db='ikegai'
+    
     collection='usecases'
+    if ENV=="PROD":
+        db='ikegai'
+    else:
+        db='ikegai_dev'
     
