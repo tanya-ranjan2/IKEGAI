a931649c72b5ab8b4cb34269a94b083ccd28b441
Bhattacharya <indreshb@kpmg.com>
2024-05-22 03:37:32 +0530

    ENV

--- app/Tools/src/rag/rag_tools.py
+++ app/Tools/src/rag/rag_tools.py
@@ -17,16 +17,23 @@ def rag(query:str,topk:int=3,**kwargs)->list:
     agent_state=kwargs['state']
     data_sourse=agent_state.config['data_sources']
     embeddings=rag_utils.load_embeddings()
-    db=rag_utils.load_vectordb(data_sourse['storage_name'],embeddings,collection_name=data_sourse['collection_name'],topk=topk)
+    all_docs=[]
+    all_refs=[]
+    for d in data_sourse["vectorDB"]:
+        db=rag_utils.load_vectordb(d['storage_name'],embeddings,collection_name=d['collection_name'],topk=topk)
     
-    documents=db.invoke(query)
-    reference=[i.metadata for i in documents]
-    info_list=[{"page":m['page'],"path":m["path"].split("/")[-1]} for m in reference]
+        documents=db.invoke(query)
+        
+        cc_context=[i.page_content for i in documents]
+        reference=[i.metadata for i in documents]
+        info_list=[{"page":m['page'],"path":m["path"].split("/")[-1]} for m in reference]
+        all_docs.extend(cc_context)
+        all_refs.extend(info_list)
 
-    agent_state.state['sources']=info_list
-    agent_state.state['context']= [i.page_content for i in documents]
-    print(rag_utils.make_context(documents))
-    return rag_utils.make_context(documents)
+    agent_state.state['sources']=all_refs
+    agent_state.state['context']= all_docs
+    print(rag_utils.make_context(all_docs))
+    return rag_utils.make_context(all_docs)
     #return "Search VectorDB"
     
 
