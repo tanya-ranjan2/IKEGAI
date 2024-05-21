4b36502e91da5a430d87f26e596c4ca706b2acff
Bhattacharya <indreshb@kpmg.com>
2024-05-21 23:17:23 +0530

    ENV

--- app/_temp/config.py
+++ app/_temp/config.py
@@ -76,7 +76,7 @@ class CeleryQueue:
     
 @dataclass
 class RedisBroker:
-    host:str="20.41.249.147"
+    host:str="{URI}"
     #host:str="127.0.0.1"
     port:int=6379
     username:str="default"
