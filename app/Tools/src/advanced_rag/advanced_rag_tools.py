cd60db1d7e9b3f947f2f0da971ca4336de929f02
Bhattacharya <indreshb@kpmg.com>
2024-05-23 10:30:29 +0530

    added eligibility

--- app/Tools/src/advanced_rag/advanced_rag_tools.py
+++ app/Tools/src/advanced_rag/advanced_rag_tools.py
@@ -23,5 +23,5 @@ def advanced_rag(user_query:str, **kwargs) -> str :
     agent_state.state["context"]=result["context"]
     print("Results:",result)
     context=result["context"]
-    print("-------------->NUM TOKENS:",len(context.split()))
+    print("-------------->NUM TOKENS:",len(context.split()),len(context))
     return result["context"]
